f1c40825bea40ff0ad289a38e8f73e53dc9dd899
Roman Mozhaev <roman.mozhaev@gmail.com>
2020-12-04 17:53:40 +0300

    10. token storing

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -12,6 +12,7 @@ import {Router} from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
 
   constructor(private auth: AuthService, private router: Router) { }
 
@@ -32,6 +33,7 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+    this.submitted = true;
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -39,6 +41,7 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
     });
   }
 }
