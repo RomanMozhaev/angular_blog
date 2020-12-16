5252c8deb1f53efa249587e25fa2570c515a1c33
Roman Mozhaev <roman.mozhaev@gmail.com>
2020-12-16 15:39:35 +0300

    21. post editing

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {PostsService} from '../../shared/posts.service';
 import {switchMap} from 'rxjs/operators';
 import {Post} from '../shared/interfaces';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
   form: FormGroup;
+  post: Post;
+  submitted = false;
+  uSub: Subscription;
 
   constructor(private route: ActivatedRoute, private postsService: PostsService) { }
 
@@ -20,6 +24,7 @@ export class EditPageComponent implements OnInit {
       .pipe(switchMap((params: Params) => {
         return this.postsService.getById(params['id']);
       })).subscribe((post: Post) => {
+        this.post = post;
         this.form = new FormGroup({
           title: new FormControl(post.title, Validators.required),
           text: new FormControl(post.text, Validators.required)
@@ -28,6 +33,22 @@ export class EditPageComponent implements OnInit {
   }
 
   submit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    this.submitted = true;
+    this.uSub = this.postsService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title
+    }).subscribe(() => {
+      this.submitted = false;
+    });
+  }
 
+  ngOnDestroy(): void {
+    if (this.uSub) {
+      this.uSub.unsubscribe();
+    }
   }
 }
